9f0c2387979774b3f93f3c44cdaf6f3c9ff488ac
JC <chenjyw@gmail.com>
2017-06-26 22:33:00 +0800

    first

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='death_by_captcha',
+      version='0.1',
+      description='Captcha solver',
+      url='http://www.deathbycaptcha.com',
+      author='Death by Captcha',
+      author_email='dbc@dbc.com',
+      license='MIT',
+      packages=['death_by_captcha'],
+      zip_safe=False)
